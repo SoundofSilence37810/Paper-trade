674e742cf4f7d0e7fd6229fd618102b1514c505a
Phil <burns37810@yahoo.com>
2021-11-28 20:02:07 -0500

    Stock price and moving average working

--- Stratagies.py
+++ Stratagies.py
@@ -1,18 +1,62 @@
 #Modules
 import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 #Test function
-def yFinance_test():
+def yFinance_test(ticker:str):
 
-    #Set the ticker to CIBC
-    cibc = yf.Ticker("CM.TO")
+    #Fixes compatibility issues with yahoo finance
+    yf.pdr_override()
 
-    #Get the stock information dictionary
-    cibc.info
+    #Set the ticker_object to ticker
+    ticker_object = yf.Ticker(ticker)
 
-    print(cibc.info)
+    #Get the stock information pandas dataframe
+    stock_info = ticker_object.info
+
+    #Get the price information as a pandas dataframe
+    price_info = ticker_object.history(period="max")
+
+    #ticker_object.
+    #print(ticker_object.dividends)
+
+    #print(ticker_object.info)
+
+    #print(price_info.loc[:,["Close"]])
+
+    #Create a new simple moving average column
+    price_info["sm_50"] = price_info.iloc[:,2].rolling(window=50).mean()
+
+    #Set the iLOC(Index Location) after the first 50 rows
+    price_info = price_info.iloc[50:]
+
+    print(price_info.columns)
+    #print(price_info.columns)
+
+    #print(price_info[3:])
+    # print(price_info)
+    #
+    # for key in price_info:
+    #     print(key,price_info[key][1])
+
+    #Display a chart
+    plt.plot(price_info.index, price_info.iloc[:,[3]],price_info.index,price_info.iloc[:,[7]])
+    plt.title(ticker)
+    plt.xlabel=('Price')
+    plt.ylabel=('Year')
+    plt.show()
 
 # Paper trade from past data
 def paper_trade():
+    pass
+
+# Determine the margin of safety
+def mos():
+    pass
+
+# Determine the number of price flucuations at a given percentage
+def flucuations():
     pass
 
--- main.py
+++ main.py
@@ -7,8 +7,23 @@ from Stratagies import *
 import yfinance as yf
 
 def main() -> int:
-    print('Hello World')
-    yFinance_test()
+
+    #Application Info
+    print(f'Paper Trades Application')
+
+    #Get input from the user
+    ticker_symbol = input('Please enter a stock symbol: ')
+
+    #Check to make certain the ticker symbol exists
+
+
+    #If the ticker symbol is valid or blank
+    if ticker_symbol:
+        yFinance_test(ticker_symbol)
+    else:
+        yFinance_test('CM.TO')
+
+    #End program with error code zero
     return 0
 
 if __name__ == '__main__':
